67194ce2e1e8c42c3980988f536f432d2c525612
Keven <huangzhekai@derucci.net>
2023-10-17 12:05:45 +0800

    Feature: turn on tmux true color support

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,9 +1,10 @@
- #!/bin/bash
+#!/bin/bash
 
- files=$(ls -a $1 | grep -E '.[^.]+' |grep -v .git|grep -v nvim|grep -v autoconfig.sh)
- echo $files
- # 去掉 ls -a 返回结果中的 ". .. .git"
- # for file in `echo $files`; do
- #     ln -s $1/$file ~/$file # 创建软链接
- # done
+files=$(ls -a $1 | grep -E '.[^.]+' |grep -v .git|grep -v nvim|grep -v autoconfig.sh)
 
+for file in `echo $files`; do
+    ln -s $1/$file ~/$file # 创建软链接
+done
+
+# 配置nvim
+ln -sf ~/.config/nvim ./nvim
