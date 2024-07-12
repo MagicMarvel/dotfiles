beef9e98a5d6e85842bbf5ebf25a73db0e1b4364
Keven <huangzhekai@derucci.net>
2024-07-12 16:53:14 +0800

    Feature: wezTerm support and update nvim plugins's version

--- nvim/lua/user/dev/init.lua
+++ nvim/lua/user/dev/init.lua
@@ -1,10 +1,22 @@
 return {
     {
         "MagicMarvel/smart-im-select.nvim",
+        enabled = false,
         dev = true,
         dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter", "rcarriga/nvim-notify" },
         config = function()
             require("im_select").setup({})
+        end,
+    },
+    {
+        "keaising/im-select.nvim",
+        enabled = false,
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        dev = true,
+        config = function()
+            require("im_select").setup({
+                smart_switch = true
+            })
         end,
     },
     { 'rafcamlet/nvim-luapad' }
