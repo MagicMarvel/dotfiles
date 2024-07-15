5f7d48e3c3ef94a4b1da180c8df283daa9ddf822
Keven <huangzhekai@derucci.net>
2024-07-15 14:21:45 +0800

    Disable transparent

--- nvim/lua/user/plugins/ui.lua
+++ nvim/lua/user/plugins/ui.lua
@@ -328,9 +328,9 @@ return {
     {
         "folke/tokyonight.nvim",
         lazy   = true,
-        -- init   = function()
-        --     vim.cmd [[colorscheme tokyonight]]
-        -- end,
+        init   = function()
+            vim.cmd [[colorscheme tokyonight]]
+        end,
         opts   = {
             -- your configuration comes here
             -- or leave it empty to use the default settings
@@ -373,9 +373,9 @@ return {
     {
         "lunarvim/darkplus.nvim",
         lazy = true,
-        init = function()
-            vim.cmd [[colorscheme darkplus]]
-        end
+        -- init = function()
+        --     vim.cmd [[colorscheme darkplus]]
+        -- end
     },
     {
         "tomasiser/vim-code-dark",
